f05cbf5ec6b09aee14e73dc7b35c4b943c2fccec
Jacobo Mateo Bedoya Oquendo <jacobomateobedoya@icloud.com>
2026-08-01 23:27:35 -0500

    CHORE: checkpoint for test
    Trying out concurrency here with the counter.add("host"). Might delete
    later, I don't know.

--- src/http.go
+++ src/http.go
@@ -4,21 +4,33 @@ import (
 	"bufio"
 	"bytes"
 	"net/http"
+	"sync"
 
 	"github.com/gopacket/gopacket"
 )
 
-// HttpCounter here is where we will store all the traffic.
+// HttpCounter here is where we will store all the traffic. Concurrent mode
+// baby
 type HttpCounter struct {
 	total int
+	mu    sync.RWMutex
 	hosts map[string]int
 }
 
 func NewCounter() *HttpCounter {
-	counter := HttpCounter{0, make(map[string]int)}
+	counter := HttpCounter{hosts: make(map[string]int)}
 	return &counter
 }
 
+// add concurrency safe add operation
+func (counter *HttpCounter) add(host string) {
+	counter.mu.Lock()
+	counter.total += 1
+	counter.hosts[host] += 1
+	counter.mu.Unlock()
+}
+
+// count count the total requests and the map
 func (counter *HttpCounter) count(pkt gopacket.Packet) {
 	appLayer := pkt.ApplicationLayer()
 	// the packet is not in this application layer
@@ -38,13 +50,6 @@ func (counter *HttpCounter) count(pkt gopacket.Packet) {
 		return
 	}
 
-	// if the request came to here MUST be valid, don't you think??
-	counter.total += 1
-
-	// not send to a host
-	if request.Host == "" {
-		counter.hosts["Unkown"] += 1
-		return
-	}
-	counter.hosts[request.Host] += 1
+	// now everything is in here
+	counter.add(request.Host)
 }
